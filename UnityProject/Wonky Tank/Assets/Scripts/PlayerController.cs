88accdb6c3fc24a942efe0ad9c79588f1f4a9027
mabdrew <mbrewer1337@gmail.com>
2017-10-13 16:11:33 -0500

    First pass of turning Alex's MVP into our UML structure -- in progress

--- UnityProject/Wonky Tank/Assets/Scripts/PlayerController.cs
+++ UnityProject/Wonky Tank/Assets/Scripts/PlayerController.cs
@@ -7,40 +7,87 @@ public class PlayerController : MonoBehaviour {
 
    // private Rigidbody rigBod;
 
-    public float playerSpeed;
-    public float rotateSpeed;
- 
+    public float PlayerSpeed;
+    public float RotateSpeed;
+
+    public KeyCode Forward;
+    public KeyCode Backward;
+    public KeyCode Left;
+    public KeyCode Right;
+
+    GameObject OwningGame;
+    byte CurrentFrame; //Q : figure out how to fix framerates. Better way to track frames?
+    public byte TankID;
+
+    byte GetTankID()
+    {
+        return TankID;
+    }
 
     // Use this for initialization
-    void Start () {	}
-	
+    void Start () 
+    {
+        Forward = KeyCode.W;
+        Backward = KeyCode.S;
+        Left = KeyCode.A;
+        Right = KeyCode.D;
+
+        //on start up find your parent game
+        var RootGMs = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects();
+        
+        foreach (var GObj in RootGMs)
+        {
+            if (GObj.CompareTag("Game"))
+            {
+                OwningGame = GObj;
+                break;
+            }
+        }
+    }
+
+    void Turn() //rotate Left or Right
+    {
+        if (Input.GetKey(Left) || Input.GetKey(Right))
+        {
+            transform.Rotate(0.0f, Input.GetAxis("Horizontal") * RotateSpeed, 0.0f);
+        }
+    }
+
+    void MoveForward()
+    {
+        //move Forward
+        if (Input.GetKey(Forward))
+        {
+            transform.position += transform.forward * Time.deltaTime * PlayerSpeed;
+            Turn(); //having a call to turn here enables turning while moving
+        }
+
+        //move Backward
+        else if (Input.GetKey(Backward))
+        {
+            transform.position -= transform.forward * Time.deltaTime * PlayerSpeed;
+            Turn(); //enable turning while moving Backward
+        }
+    }
+
+    void MoveTank(byte[] fno_and_tid)
+    {
+        if (fno_and_tid[1] != TankID)
+            return;//if your tank id doesn't match, then ignore the message to move
+        MoveForward();
+        Turn();
+    }  
+
 	// Update is called once per frame
 	void FixedUpdate ()
     {
-        //move forward
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position += transform.forward * Time.deltaTime * playerSpeed;
-            turn(); //having a call to turn here enables turning while moving
-        }
 
-        //move backward
-        else if (Input.GetKey(KeyCode.S))
-        {
-            transform.position -= transform.forward * Time.deltaTime * playerSpeed;
-            turn(); //enable turning while moving backward
-        }
+        byte[] FrameNumberAndTankID = {CurrentFrame,TankID};
+        gameObject.SendMessage("MoveTank", FrameNumberAndTankID);
 
-        //rotate left or right while not movin'
-        turn(); //I think tanks can do that anyway
-    }
-
-    void turn() //rotate left or right
-    {
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(0.0f, Input.GetAxis("Horizontal") * rotateSpeed, 0.0f);
-        }
+        CurrentFrame++;
+        if (CurrentFrame == 30)
+            CurrentFrame = 0;
     }
 
     void OnTriggerEnter(Collider other)
