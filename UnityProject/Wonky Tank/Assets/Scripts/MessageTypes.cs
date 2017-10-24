93d3fee15a56d826040c9c31561d7660aced7d1c
mabdrew <mbrewer1337@gmail.com>
2017-10-24 18:15:30 -0500

    wip

--- UnityProject/Wonky Tank/Assets/Scripts/MessageTypes.cs
+++ UnityProject/Wonky Tank/Assets/Scripts/MessageTypes.cs
@@ -43,7 +43,7 @@ class DamageTankMsg
     }
 
     public byte TankID;
-    int FrameNo;
+    public int FrameNo;
     public float Amount; 
 }
 
---
+++ UnityProject/Wonky Tank/Assets/Scripts/TanksScripts/GunController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+
+    public GameObject OwningTank;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 NewPos = OwningTank.transform.position;
+        NewPos.y += 0.5f;//MAGIC NUMBER
+		transform.position = NewPos;
+        //Quaternion NewRot = OwningTank.transform.rotation;
+        //NewRot.x = transform.rotation.x;
+        Quaternion NewRot = transform.rotation;
+        transform.rotation = NewRot;
+	}
+}
