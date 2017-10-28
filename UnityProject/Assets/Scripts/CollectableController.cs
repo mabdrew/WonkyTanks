8499096940b6fb435179d5e1ed43f1864ed82b35
arooz <alexrooz1990@gmail.com>
2017-10-28 17:30:44 -0500

    Basic Collectables Implemented
    I hope this is the right format.
    Basic collectables that rotate and disappear when intersected with
    have been implemented. This includes their material, as well as changes
    to layers and tags. A new "Collectable" layer has been added that does not
    collide with the tank "feet" and any part of the tank body, which the
    collectables have been assigned to. The tank's cube and nose have been
    added to the "Player" layer for this reason as well. The Collectables also
    have the tag "Collectable" but this can probably be removed. Lastly,
    unless the code can changed, only the "TankBody_1" object should have the
    tag "Player", as the CollectableController script relies on this
    to find the Player GameObject. The CollectableController script rotates
    the Collectables and checks for intersection with the Player bounds.

---
+++ UnityProject/Assets/Scripts/CollectableController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableController : MonoBehaviour {
+
+    private GameObject playerObject;
+    private GameObject collectableObject;
+
+    private Collider playerCollider;
+    private Collider collectableCollider;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerObject = GameObject.FindGameObjectWithTag("Player"); //find player. hopefully nothing else gets the "Player" tag
+        collectableObject = gameObject; //get collectable game object
+
+        if (playerObject != null)
+            playerCollider = playerObject.GetComponent<Collider>(); //get collider for intersection detection
+
+        if (collectableObject != null)
+            collectableCollider = collectableObject.GetComponent<Collider>(); //get collider for intersection detection
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.Rotate(new Vector3(360, 180, 0) * Time.deltaTime); //rotate the collectable
+
+        if (collectableCollider.bounds.Intersects(playerCollider.bounds)) //check for intersection with Player
+            //player and collectables don't collide with each other
+        {
+            collectableObject.gameObject.SetActive(false); //make collectable disappear
+        }
+    }
+}
