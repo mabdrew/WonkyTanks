6f2fd7794c030c2752f3f865c4f7bf83d941deea
mabdrew <mbrewer1337@gmail.com>
2017-11-06 19:58:49 -0600

    Simple Guardian that tracks player.

---
+++ UnityProject/Assets/Scripts/EnemyScripts/Guardian.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guardian : MonoBehaviour {
+
+    GameObject OwningGame;
+
+    public GameObject TestPlayer;//for testing purposes only
+    const float NoticeDistance = 5f;
+
+
+	// Use this for initialization
+	void Start () {
+        GameUtilities.FindGame(ref OwningGame);
+	}
+	
+
+    void LookAtTestPlayer()
+    {
+        if(TestPlayer!=null)
+        {
+            transform.LookAt(TestPlayer.transform);
+            print("looky!");
+        }
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+		LookAtTestPlayer();
+	}
+}
