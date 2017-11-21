7579c782c0cbf4d90ee6f934bad2883e41ea999e
arooz <alexrooz1990@gmail.com>
2017-11-21 06:25:18 -0600

    Timer In Place
    Timer with it's UI components are in place and it counts down.
    Fail trigger will be added in the future.

---
+++ UnityProject/Assets/Scripts/Timer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    private float totalTime = 120.0f;
+
+    [SerializeField] private Text TimerText;
+
+    public float TotalTime
+    {
+        get
+        {
+            return totalTime;
+        }
+
+        set
+        {
+            totalTime = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        TotalTime -= Time.deltaTime;
+
+        if(TotalTime <= 0.0f)
+        {
+            //fail
+        }
+        else
+        {
+            TimerText.text = string.Format("{0}:{1:0.00}", (int)(TotalTime / 60), TotalTime % 60);
+        }
+	}
+
+    private void FailLevel()
+    {
+
+    }
+}
