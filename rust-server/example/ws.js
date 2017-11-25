7cb06ae3e720662c221e08844ac5ba2cc146d08f
Wesley Wolfe <wesley.d.wolfe+git@gmail.com>
2017-11-25 11:42:23 -0600

    Add basic testing example for server

---
+++ rust-server/example/ws.js
@@ -0,0 +1,45 @@
+(() => {
+    let ws = new WebSocket("ws://" + location.host + "/chat");
+    let mb = document.getElementById('responses');
+    let input = document.getElementById('input');
+    let encoder = new TextEncoder("utf-8");
+    let decoder = new TextDecoder("utf-8");
+    ws.onopen = function() {
+        log('debug', "Connected");
+        input.style.visibility = 'visible';
+    };
+
+    ws.onclose = function() {
+        input.style.visibility = 'hidden';
+        log('warning', "Disconnected");
+    };
+
+    ws.onerror = function(e) {
+        input.style.visibility = 'hidden';
+        log('warning', 'ERROR: ' + e);
+    };
+    ws.onmessage = function(ev) {
+        if ("string" === typeof ev.data) {
+            log('meta', ev.data);
+        } else {
+            let reader = new FileReader();
+            reader.onload = (event) => log('text', decoder.decode(event.target.result));
+            reader.readAsArrayBuffer(ev.data)
+        }
+    };
+    input.onkeydown = function(ev) {
+        if(ev.which === 13) {
+            ws.send(encoder.encode(input.value));
+            log('self', input.value);
+            input.value = '';
+        }
+    };
+
+    function log(type, message) {
+        let red = document.createElement('div');
+        red.className = type;
+        red.appendChild(document.createTextNode(message));
+        mb.insertBefore(red, mb.childNodes[0]);
+    }
+
+})();
