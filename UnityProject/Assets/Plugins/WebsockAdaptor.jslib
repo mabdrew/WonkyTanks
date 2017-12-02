7dd7d1b37779fec8b1192e744629b6edbd37d1f8
Wesley Wolfe <wesley.d.wolfe+git@gmail.com>
2017-12-02 15:22:40 -0600

    Add network changes

---
+++ UnityProject/Assets/Plugins/WebsockAdaptor.jslib
@@ -0,0 +1,59 @@
+﻿var LibraryWebsockAdaptor = {
+$commons: {},
+$game: {},
+
+// SendMessage('Game', 'FunctionName', ...);
+
+WebsockAdaptorStart: function() {
+	var path = window.location.pathname;
+	path = path.substr(0, path.lastIndexOf("/") + 1);
+	var lobby = commons.lobby = new WebSocket("ws://" + location.host + path);
+	var encoder = commons.encoder.new TextEncoder("utf-8");
+	var decoder = commons.decoder.new TextDecoder("utf-8");
+	lobby.onopen = function() {
+		// FIXME
+	};
+	lobby.onclose = function() {
+		// FIXME
+	};
+	lobby.onmessage = function(ev) {
+		if ("string" === typeof ev.data) {
+			var ADD_CHANNEL_TOKEN = "+: ";
+			if (ev.data.startsWith(ADD_CHANNEL_TOKEN)) {
+				SendMessage('Game', 'NewChannel', ev.data.substr(ADD_CHANNEL_TOKEN.length));
+			}
+			var REMOVE_CHANNEL_TOKEN = "-: ";
+			if (ev.data.startsWith(REMOVE_CHANNEL_TOKEN)) {
+				SendMessage('Game', 'RemoveChannel', ev.data.substr(REMOVE_CHANNEL_TOKEN.length));
+			}
+		}
+	};
+	var room = commons.room = new WebSocket("ws://" + location.host + "/example");
+	room.onopen = function() {
+		// FIXME
+	};
+	room.onclose = function() {
+		// FIXME
+	};
+	room.onmessage = function(ev) {
+		if ("string" === typeof ev.data) {
+		} else {
+			let reader = new FileReader();
+			reader.onload = function (event) {
+				SendMessage('Game', 'ReceivePacket', decoder.decode(event.target.result));
+			}
+			reader.readAsArrayBuffer(ev.data)
+		}
+	};
+},
+
+WebsockAdaptorSend: function(str_ptr) {
+	let str = Pointer_stringify(str_ptr);
+	commons.room.send(encoder.encode(str));
+}
+
+};
+
+autoAddDeps(LibraryWebsockAdaptor, '$commons');
+autoAddDeps(LibraryWebsockAdaptor, '$game');
+mergeInto(LibraryManager.library, LibraryWebsockAdaptor);
---
+++ UnityProject/Assets/Scripts/WebsockAdaptor.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using TankMessages;
+
+
+public class WebsockAdaptor : MonoBehaviour {
+
+	[DllImport("__Internal")]
+	private static extern void WebsockAdaptorStart();
+	[DllImport("__Internal")]
+	private static extern void WebsockAdaptorSend(string msg);
+
+	// Use this for initialization
+	void Start () {
+		WebsockAdaptorStart ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void NewChannel (string name) {
+		print ("Added channel:: " + name);
+	}
+
+	void RemoveChannel (string name) {
+		print ("Removed channel:: " + name);
+	}
+
+	void ReceivePacket(string data) {
+		GameObject OwningGame = null;
+		GameUtilities.FindGame(ref OwningGame);
+		string[] id_data_pair = data.Split (new char[]{','}, 2);
+		switch (int.Parse(id_data_pair[0])) {
+		case MoveGunVerticalID:
+			OwningGame.SendMessage ("MoveGunVertical", ReconstructTankComponentMovementMsg (id_data_pair [1]));
+			break;
+		case MoveGunHorizontalID:
+			OwningGame.SendMessage ("MoveGunHorizontal", ReconstructTankComponentMovementMsg (id_data_pair [1]));
+			break;
+		case MoveTankID:
+			OwningGame.SendMessage ("MoveTank", ReconstructTankComponentMovementMsg (id_data_pair [1]));
+			break;
+		case TurnTankID:
+			OwningGame.SendMessage ("TurnTank", ReconstructTankComponentMovementMsg (id_data_pair [1]));
+			break;
+		case StrafeTankID:
+			OwningGame.SendMessage ("StrafeTank", ReconstructTankComponentMovementMsg (id_data_pair [1]));
+			break;
+		default:
+			// No-op?
+			break;
+		}
+	}
+
+	private const int MoveGunVerticalID = 1;
+	void MoveGunVertical(TankComponentMovementMsg msg) {
+		WebsockAdaptorSend (DeconstructTankComponentMovementMsg(MoveGunVerticalID, msg));
+	}
+
+	private const int MoveGunHorizontalID = MoveGunVerticalID + 1;
+	void MoveGunHorizontal(TankComponentMovementMsg msg) {
+		WebsockAdaptorSend (DeconstructTankComponentMovementMsg(MoveGunHorizontalID, msg));
+	}
+
+	private const int TurnTankID = MoveGunHorizontalID + 1;
+	void TurnTank(TankComponentMovementMsg msg) {
+		WebsockAdaptorSend (DeconstructTankComponentMovementMsg(TurnTankID, msg));
+	}
+
+	private const int MoveTankID = TurnTankID + 1;
+	void MoveTank(TankComponentMovementMsg msg) {
+		WebsockAdaptorSend (DeconstructTankComponentMovementMsg(MoveTankID, msg));
+	}
+
+	private const int StrafeTankID = MoveTankID + 1;
+	void StrafeTank(TankComponentMovementMsg msg) {
+		WebsockAdaptorSend (DeconstructTankComponentMovementMsg(StrafeTankID, msg));
+	}
+
+	static TankComponentMovementMsg ReconstructTankComponentMovementMsg(string message) {
+		string[] parts = message.Split (new char[]{','});
+		TankComponentMovementMsg msg = new TankComponentMovementMsg ();
+		msg.Direction = bool.Parse (parts [0]);
+		msg.FrameNo = int.Parse (parts [1]);
+		msg.TankID = byte.Parse (parts [2]);
+		return msg;
+	}
+
+	static string DeconstructTankComponentMovementMsg(int id, TankComponentMovementMsg msg) {
+		return
+			id
+			+ "," + msg.Direction
+			+ "," + msg.FrameNo
+			+ "," + msg.TankID;
+	}
+}
